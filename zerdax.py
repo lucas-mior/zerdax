6f24d1c0cf5d249f8bf05e6f4ab1c1d04b9890cc
lucas-mior <lucas.mior@tutamail.com>
2022-06-08 09:47:22 -0300

    parse args

--- zerdax.py
+++ zerdax.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python
 
+import argparse
+
+def parseargs():
+    parser = argparse.ArgumentParser(description='Convert chess photo to FEN')
+    parser.add_argument('image', type=str, help='Nome do arquivo da foto')
+    args = parser.parse_args()
+    return args
+
 def Main():
-    print("Zerdax")
+    args = parseargs()
+    image = args.image
+    print("Processing: {}".format(image))
 
 if __name__ == '__main__':
     Main()
