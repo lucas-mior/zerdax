46508dd3a583dc40043a4035b57357072bc54038
lucas-mior <lucas.mior@tutamail.com>
2022-06-08 10:11:25 -0300

    dummy algorithm

---
+++ algo.py
@@ -0,0 +1,7 @@
+def full(image):
+    """ given a file path to a chessboard image,
+        returns a FEN notation
+    """
+    predicted_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
+    print("algo.full: {}".format(image))
+    return predicted_fen
--- zerdax.py
+++ zerdax.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import argparse
+import algo
 
 def parseargs():
     parser = argparse.ArgumentParser(description='Convert chess photo to FEN')
@@ -12,6 +13,8 @@ def Main():
     args = parseargs()
     image = args.image
     print("Processing: {}".format(image))
+    fen = algo.full(image)
+    print(fen)
 
 if __name__ == '__main__':
     Main()
