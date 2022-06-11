736fd8b1da30d491789f0f3e87f57f2ac82116b9
lucas-mior <lucas.mior@tutamail.com>
2022-06-10 21:34:12 -0300

    opencv will be used

--- algo.py
+++ algo.py
@@ -1,7 +1,12 @@
+import cv2
+
 def full(image):
     """ given a file path to a chessboard image,
         returns a FEN notation
     """
     predicted_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
     print("algo.full: {}".format(image))
+    cvcolor = cv2.imread(image)
+    cvgray = cv2.cvtColor(cvcolor, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite("{}.jpg".format(image), cvgray)
     return predicted_fen
