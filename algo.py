f7e89906ac4a6ecf994a791ee08c7e28f4a21bb4
lucas-mior <lucas.mior@tutamail.com>
2022-07-03 12:29:18 -0300

    update testes

--- algo.py
+++ algo.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-def testes(cvgray):
+def testes(name, cvgray):
     scale_percent = 25
     width = int(cvgray.shape[1] * scale_percent / 100)
     height = int(cvgray.shape[0] * scale_percent / 100)
@@ -20,11 +20,12 @@ def testes(cvgray):
 
     cveq = cv2.equalizeHist(cvgray)
 
+    ts = 5
+    cl = 2 
+
     cvgauss = cv2.GaussianBlur(cvgray, [5,5], 0)
     cvmedian = cv2.medianBlur(cvgray, 5)
 
-    ts = 5
-    cl = 2 
     clahe = cv2.createCLAHE(clipLimit=float(cl), tileGridSize=(ts,ts))
 
     cvclahe_before = clahe.apply(cvgray)
@@ -33,25 +34,25 @@ def testes(cvgray):
 
     # cv2.findChessboardCorners()
 
-    cv2.imwrite('cvgauss.jpg', cvgauss)
-    cv2.imwrite('cvmedian.jpg', cvmedian)
-    cv2.imwrite('cvclahe_aftermedian.jpg', cvclahe_after)
-    cv2.imwrite('cvclahe_beforemedian.jpg', cvclahe_before)
-    cv2.imwrite('cveq.jpg', cveq)
-    cv2.imwrite('cvthr.jpg', cvthr)
-    cv2.imwrite('cvhf.jpg', cvhf)
-    cv2.imwrite('cvres.jpg', cvres) 
-    cv2.imwrite('cvgray.jpg', cvgray)
+    # cv2.imwrite('cvgauss.jpg', cvgauss)
+    # cv2.imwrite('cvmedian.jpg', cvmedian)
+    cv2.imwrite('{}clahe.jpg'.format(name), cvclahe_after)
+    # cv2.imwrite('cvclahe_beforemedian.jpg', cvclahe_before)
+    # cv2.imwrite('cveq.jpg', cveq)
+    # cv2.imwrite('cvthr.jpg', cvthr)
+    # cv2.imwrite('cvhf.jpg', cvhf)
+    # cv2.imwrite('cvres.jpg', cvres) 
+    # cv2.imwrite('cvgray.jpg', cvgray)
 
 def full(image):
     """ given a file path to a chessboard image,
         returns a FEN notation
     """
-    predicted_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
     print("algo.full: {}".format(image))
     cvcolor = cv2.imread(image)
     cvgray = cv2.cvtColor(cvcolor, cv2.COLOR_BGR2GRAY)
 
-    testes(cvgray)
+    testes(image, cvgray)
 
+    predicted_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
     return predicted_fen
