09e1d8fbb125ccb0650d5025cc8292895e1c27e8
lucas-mior <lucas.mior@tutamail.com>
2022-07-02 21:02:29 -0300

    testes iniciais com funções básicas

--- algo.py
+++ algo.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 def full(image):
     """ given a file path to a chessboard image,
@@ -15,7 +16,19 @@ def full(image):
     dsize = (width, height)
     cvres = cv2.resize(cvgray, dsize)
 
-    cv2.imwrite('resize.jpg', cvres) 
-    cv2.imwrite('gray.jpg', cvgray)
+    kernel = np.array([[-1.0, -1.0, 2.0], 
+                       [-1.0, 2.0, -1.0],
+                       [2.0, -1.0, -1.0]])
+
+    kernel = kernel / (np.sum(kernel) if np.sum(kernel) != 0 else 1)
+
+    cvhf = cv2.filter2D(cvres, -1, kernel)
+
+    ret, cvthr = cv2.threshold(cvres, 160, 255, cv2.THRESH_BINARY)
+
+    cv2.imwrite('cvthr.jpg', cvthr)
+    cv2.imwrite('cvhf.jpg', cvhf)
+    cv2.imwrite('cvres.jpg', cvres) 
+    cv2.imwrite('cvgray.jpg', cvgray)
 
     return predicted_fen
