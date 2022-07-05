cbe99eb975978f660842965f09f45d7c5518104b
lucas-mior <lucas.mior@tutamail.com>
2022-07-05 19:59:24 -0300

    removed debug printfs

--- libwang.c
+++ libwang.c
@@ -16,13 +16,10 @@ double weight(double *F, int x, int y, int xx, int yy) {
 }
 
 void weight_array(double *F, int xx, int yy, double *W) {
-    printf("weight_array(%p, %d, %d)\n", F, xx, yy);
-
     for (int x = 1; x < xx-1; x++) {
         for (int y = 1; y < yy-1; y++) {
             W[yy*x + y] = weight(F, x, y, xx, yy);
         }
     }
-    printf("C: W[%d*200 + 200] = %f\n", xx, W[yy*200 + 150]);
     return;
 }
