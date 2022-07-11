f680f3da98db9a950ab669f2cbe7aea904423212
lucas-mior <lucas.mior@tutamail.com>
2022-07-11 14:00:38 -0300

    fix arg parse

--- src/Image.py
+++ src/Image.py
@@ -3,4 +3,4 @@ from pathlib import Path
 class Image:
     def __init__(self, filename):
         self.filename = filename
-        self.basename = Path(filename).stem
+        self.basename = Path(self.filename).stem
--- src/zerdax.py
+++ src/zerdax.py
@@ -5,20 +5,20 @@ import algo
 
 def parseargs():
     parser = argparse.ArgumentParser(description='Convert chess photo to FEN')
-    parser.add_argument('image', type=str, help='Nome do arquivo da foto')
-    parser.add_argument('-a', nargs='?', type=int, default=20,  help='Canny: low threshold')
-    parser.add_argument('-b', nargs='?', type=int, default=120, help='Canny: high threshold')
-    parser.add_argument('-c', nargs='?', type=int, default=60,  help='Hough: votes')
-    parser.add_argument('-d', nargs='?', type=int, default=150, help='Hough: minLen')
-    parser.add_argument('-e', nargs='?', type=int, default=15,  help='Hough: maxGap')
+    parser.add_argument('image',              type=str, default=None, help='Nome do arquivo da foto')
+    parser.add_argument('-c_thrl', nargs='?', type=int, default=20,   help='Canny: low threshold')
+    parser.add_argument('-c_thrh', nargs='?', type=int, default=120,  help='Canny: high threshold')
+    parser.add_argument('-h_thrv', nargs='?', type=int, default=60,   help='Hough: minimum votes')
+    parser.add_argument('-h_minl', nargs='?', type=int, default=150,  help='Hough: minimum line length')
+    parser.add_argument('-h_maxg', nargs='?', type=int, default=15,   help='Hough: maximum gap')
     args = parser.parse_args()
     return args
 
 def Main():
     args = parseargs()
     image = args.image
-    print("Processing: {}".format(image))
-    fen = algo.full(image, args.a, args.b, args.c, args.d, args.e)
+    print("======= Zerdax {} =======".format(image))
+    fen = algo.full(image, args.c_thrl, args.c_thrh, args.h_thrv, args.h_minl, args.h_maxg)
     print(fen)
 
 if __name__ == '__main__':
