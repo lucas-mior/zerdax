521fc77b69d99dbaa8c27fb18e2749208d640f7d
lucas-mior <lucas.mior@tutamail.com>
2022-07-11 21:35:52 -0300

    remove shortest()

--- src/find_board.py
+++ src/find_board.py
@@ -12,113 +12,6 @@ import matplotlib.pyplot as plt
 import ctypes as ct
 from numpy.ctypeslib import ndpointer as ndp
 
-def closestDistanceBetweenLines(a0,a1,b0,b1,clampAll=False,clampA0=False,clampA1=False,clampB0=False,clampB1=False):
-    a0 = np.array(a0)
-    a1 = np.array(a1)
-    b0 = np.array(b0)
-    b1 = np.array(b1)
-
-    ''' Given two lines defined by numpy.array pairs (a0,a1,b0,b1)
-        Return the closest points on each segment and their distance
-    '''
-
-    # If clampAll=True, set all clamps to True
-    if clampAll:
-        clampA0=True
-        clampA1=True
-        clampB0=True
-        clampB1=True
-
-
-    # Calculate denomitator
-    A = a1 - a0
-    B = b1 - b0
-    magA = np.linalg.norm(A)
-    magB = np.linalg.norm(B)
-    
-    _A = A / magA
-    _B = B / magB
-    
-    cross = np.cross(_A, _B);
-    denom = np.linalg.norm(cross)**2
-    
-    
-    # If lines are parallel (denom=0) test if lines overlap.
-    # If they don't overlap then there is a closest point solution.
-    # If they do overlap, there are infinite closest positions, but there is a closest distance
-    if not denom:
-        d0 = np.dot(_A,(b0-a0))
-        
-        # Overlap only possible with clamping
-        if clampA0 or clampA1 or clampB0 or clampB1:
-            d1 = np.dot(_A,(b1-a0))
-            
-            # Is segment B before A?
-            if d0 <= 0 >= d1:
-                if clampA0 and clampB1:
-                    if np.absolute(d0) < np.absolute(d1):
-                        return a0,b0,np.linalg.norm(a0-b0)
-                    return a0,b1,np.linalg.norm(a0-b1)
-                
-                
-            # Is segment B after A?
-            elif d0 >= magA <= d1:
-                if clampA1 and clampB0:
-                    if np.absolute(d0) < np.absolute(d1):
-                        return a1,b0,np.linalg.norm(a1-b0)
-                    return a1,b1,np.linalg.norm(a1-b1)
-                
-                
-        # Segments overlap, return distance between parallel segments
-        return None,None,np.linalg.norm(((d0*_A)+a0)-b0)
-        
-    
-    
-    # Lines criss-cross: Calculate the projected closest points
-    t = (b0 - a0);
-    detA = np.linalg.det([t, _B, cross])
-    detB = np.linalg.det([t, _A, cross])
-
-    t0 = detA/denom;
-    t1 = detB/denom;
-
-    pA = a0 + (_A * t0) # Projected closest point on segment A
-    pB = b0 + (_B * t1) # Projected closest point on segment B
-
-
-    # Clamp projections
-    if clampA0 or clampA1 or clampB0 or clampB1:
-        if clampA0 and t0 < 0:
-            pA = a0
-        elif clampA1 and t0 > magA:
-            pA = a1
-        
-        if clampB0 and t1 < 0:
-            pB = b0
-        elif clampB1 and t1 > magB:
-            pB = b1
-            
-        # Clamp projection A
-        if (clampA0 and t0 < 0) or (clampA1 and t0 > magA):
-            dot = np.dot(_B,(pA-b0))
-            if clampB0 and dot < 0:
-                dot = 0
-            elif clampB1 and dot > magB:
-                dot = magB
-            pB = b0 + (_B * dot)
-    
-        # Clamp projection B
-        if (clampB0 and t1 < 0) or (clampB1 and t1 > magB):
-            dot = np.dot(_A,(pB-a0))
-            if clampA0 and dot < 0:
-                dot = 0
-            elif clampA1 and dot > magA:
-                dot = magA
-            pA = a0 + (_A * dot)
-
-    
-    return pA,pB,np.linalg.norm(pA-pB)
-
 def det(a, b):
     return a[0]*b[1] - a[1]*b[0]
 
@@ -312,18 +205,6 @@ def find_board(img, c_thl, c_thh, h_th, h_minl, h_maxg):
     aux[:,0, 0:6] = np.copy(A[:,0, 0:6])
     A = np.copy(aux)
     np.set_printoptions(linewidth=160)
-
-    for a in A[:,0,:]:
-        sh = 1000
-        for b in B[:,0,:]:
-            cl = closestDistanceBetweenLines([a[0],a[1],0],[a[2],a[3],0],[b[0],b[1],0],[b[2],b[3],0])
-            if sh > cl[2]:
-                sh = cl[2]
-        print("small: ", sh)
-        A[i,0,6] = sh
-        i += 1
-    print("A: ", A)
-    exit()
 
     join = np.concatenate((A,B))
     # join = np.empty((newlines.shape[0], 1, 4), dtype='int32')
