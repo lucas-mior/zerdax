a96c104942f16e602e03c59eb1f32368dd8af279
lucas-mior <lucas.mior@tutamail.com>
2022-07-26 15:26:52 -0300

    tunar find_squares

--- src/find_squares.py
+++ src/find_squares.py
@@ -14,8 +14,11 @@ import lwang
 
 def find_squares(img):
     img = perspective_transform(img)
+    img.warped3ch = cv2.cvtColor(img.warped, cv2.COLOR_GRAY2BGR)
 
-    img.wwang = lwang.wang_filter(img.warped)
+    # img.wwang = lwang.wang_filter(img.warped)
+    # save(img, "wwang", img.wwang)
+
     img.wcanny = find_wcanny(img, wmin = 12)
     save(img, "wcanny", img.wcanny)
 
@@ -48,7 +51,7 @@ def find_wcanny(img, wmin = 12):
     c_thrh = c_thrh0
 
     while c_thrh > 20:
-        img.wcanny = cv2.Canny(img.wwang, c_thrl, c_thrh)
+        img.wcanny = cv2.Canny(img.warped, c_thrl, c_thrh)
         w = img.wcanny.mean()
         if w > wmin:
             print("{0:0=.2f} > {1}, @ {2}, {3}".format(w, wmin, c_thrl, c_thrh))
@@ -64,22 +67,22 @@ def find_wcanny(img, wmin = 12):
 
 def w_lines(img):
     got_hough = False
-    h_minl0 = round((img.wwidth)*0.8)
-    h_thrv0 = round(h_minl0 / 1.5)
-    h_maxg0 = round(h_minl0 / 50) + 5
-    h_angl0 = np.pi / 360
+    h_minl0 = round((img.wwidth)*0.9)
+    h_thrv0 = round(h_minl0 / 5)
+    h_maxg0 = round(h_minl0 / 50) + 20
+    h_angl0 = np.pi / 120
 
     h_maxg = h_maxg0
     h_minl = h_minl0
     h_thrv = h_thrv0
     h_angl = h_angl0
     j = 0
-    while h_angl < (np.pi / 45):
-        lines = cv2.HoughLinesP(img.wwang, 1, h_angl, h_thrv,  None, h_minl, h_maxg)
+    while h_angl < (np.pi / 30):
+        lines = cv2.HoughLinesP(img.wcanny, 1, h_angl, h_thrv,  None, h_minl, h_maxg)
         if lines is not None:
             lines = radius_theta(lines)
             lines = filter_90(img, lines)
-            if lines.shape[0] >= 10:
+            if lines.shape[0] >= 18:
                 print("{0} lines @ {1:1=.4f}º, {2}, {3}, {4}".format(lines.shape[0],180*(h_angl/np.pi), h_thrv, h_minl, h_maxg))
                 got_hough = True
                 break
@@ -87,28 +90,30 @@ def w_lines(img):
         if lines is not None:
             print("{0} lines @ {1:1=.4f}º, {2}, {3}, {4}".format(lines.shape[0],180*(h_angl/np.pi), h_thrv, h_minl, h_maxg))
         if h_minl > h_minl0 / 5:
-            h_minl -= 3
-            h_thrv = round(h_minl / 1.5)
-            h_maxg = round(h_minl / 50) + 5
+            h_minl -= 1
+            h_thrv = round(h_minl / 5)
+            h_maxg = round(h_minl / 50) + 20
         j += 1
         h_angl += np.pi / 14400
 
     if got_hough:
         drawn_lines = cv2.cvtColor(img.warped, cv2.COLOR_GRAY2BGR) * 0
         draw_lines = cv2.cvtColor(img.warped, cv2.COLOR_GRAY2BGR) * 0
+
         for line in lines:
             for x1,y1,x2,y2,r,t in line:
                 cv2.line(draw_lines,(x1,y1),(x2,y2),(0,0,255),round(2/img.sfact))
-        drawn_lines = cv2.addWeighted(img.warped, 0.5, draw_lines, 0.8, 0)
+        drawn_lines = cv2.addWeighted(img.warped3ch, 0.5, draw_lines, 0.8, 0)
         save(img, "hough_warped", drawn_lines)
 
-        inter = find_intersections(img, lines[:,0,:])
+        # inter = find_intersections(img, lines[:,0,:])
 
-        drawn_circles = np.copy(img.warped) * 0
-        for p in inter:
-            cv2.circle(drawn_circles, p, radius=7, color=(255, 0, 0), thickness=-1)
-        drawn_circles = cv2.addWeighted(img.warped, 0.5, drawn_circles, 0.8, 0)
-        save(img, "intersections".format(img.basename), drawn_circles)
+        # drawn_circles = np.copy(img.warped) * 0
+        # for p in inter:
+        #     cv2.circle(drawn_circles, p, radius=7, color=(255, 0, 0), thickness=-1)
+        # drawn_circles = cv2.addWeighted(img.warped, 0.5, drawn_circles, 0.8, 0)
+        # save(img, "intersections".format(img.basename), drawn_circles)
+        inter = [100, 100]
     else:
         print("FAILED @ {}, {}, {}, {}".format(180*(h_angl/np.pi), h_thrv, h_minl, h_maxg))
 
@@ -121,7 +126,7 @@ def filter_90(img, lines):
     i = 0
     for line in lines:
         for x1,y1,x2,y2,r,t in line:
-            if abs(t - 90) > 10 and abs(t + 90) > 10 and abs(t) > 10:
+            if abs(t - 90) > 5 and abs(t + 90) > 5 and abs(t) > 5:
                 rem[i] = 1
             else:
                 rem[i] = 0
