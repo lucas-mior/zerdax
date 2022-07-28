348411b1e72d2ce24612d523ffc2d20b48d3778f
lucas-mior <lucas.mior@tutamail.com>
2022-07-27 21:57:06 -0300

    find 9 or 10 hori and vert

--- src/find_squares.py
+++ src/find_squares.py
@@ -21,10 +21,23 @@ def find_squares(img):
     # save(img, "wwang", img.wwang)
 
     img.wcanny = find_wcanny(img, wmin = 12)
-    # save(img, "wcanny", img.wcanny)
 
-    lines = w_lines(img)
-    lines = geo_lines(img, lines)
+    vert,hori = w_lines(img)
+
+    drawn_lines = cv2.cvtColor(img.warped, cv2.COLOR_GRAY2BGR) * 0
+    draw_lines = cv2.cvtColor(img.warped, cv2.COLOR_GRAY2BGR) * 0
+
+    for line in vert:
+        for x1,y1,x2,y2,r,t in line:
+            cv2.line(draw_lines,(x1,y1),(x2,y2),(255,0,0),round(2/img.sfact))
+    for line in hori:
+        for x1,y1,x2,y2,r,t in line:
+            cv2.line(draw_lines,(x1,y1),(x2,y2),(0,255,0),round(2/img.sfact))
+    drawn_lines = cv2.addWeighted(img.warped3ch, 0.5, draw_lines, 0.7, 0)
+    save(img, "vert_hori", drawn_lines)
+
+    # distv, disth = get_distances(vert,hori)
+
     return img
 
 def perspective_transform(img):
@@ -70,8 +83,8 @@ def find_wcanny(img, wmin = 12):
 def w_lines(img):
     got_hough = False
     h_minl0 = round((img.wwidth)*0.7)
-    h_thrv0 = round(h_minl0 / 2)
-    h_maxg0 = round(h_minl0 / 50) + 50
+    h_thrv0 = round(h_minl0 / 1.5)
+    h_maxg0 = round(h_minl0 / 50) + 60
     h_angl0 = np.pi / 120
 
     tuned = 0
@@ -80,6 +93,7 @@ def w_lines(img):
     h_thrv = h_thrv0
     h_angl = h_angl0
     j = 0
+    minlines = 10
     while h_angl < (np.pi / 10):
         th = 180*(h_angl/np.pi)
         lines = cv2.HoughLinesP(img.wcanny, 1, h_angl, h_thrv,  None, h_minl, h_maxg)
@@ -90,7 +104,8 @@ def w_lines(img):
                 bundler = HoughBundler() 
                 lines = bundler.process_lines(lines) 
                 lines = radius_theta(lines)
-                if lines.shape[0] >= 18:
+                vert,hori = geo_lines(img,lines)
+                if vert.shape[0] >= minlines and hori.shape[0] >= minlines:
                     print("{0} lines @ {1:1=.4f}º, {2}, {3}, {4}".format(lines.shape[0],th, h_thrv, h_minl, h_maxg))
                     got_hough = True
                     break
@@ -100,28 +115,21 @@ def w_lines(img):
                 print("{0} lines @ {1:1=.4f}º, {2}, {3}, {4}".format(lines.shape[0],180*(h_angl/np.pi), h_thrv, h_minl, h_maxg))
         j += 1
         h_angl += np.pi / 1800
-        if h_angl > (np.pi / 20) and tuned < 10:
+        if h_angl > (np.pi / 20) and tuned < 20:
             h_angl = h_angl0
-            h_minl -= 5
-            h_thrv -= 10
+            h_minl -= 1
+            h_thrv -= 12
             h_maxg += 5
             tuned += 1
+            if tuned > 12:
+                minlines = 9
 
-    if got_hough:
-        drawn_lines = cv2.cvtColor(img.warped, cv2.COLOR_GRAY2BGR) * 0
-        draw_lines = cv2.cvtColor(img.warped, cv2.COLOR_GRAY2BGR) * 0
-
-        for line in lines:
-            for x1,y1,x2,y2,r,t in line:
-                cv2.line(draw_lines,(x1,y1),(x2,y2),(0,0,255),round(2/img.sfact))
-        drawn_lines = cv2.addWeighted(img.warped3ch, 0.5, draw_lines, 0.8, 0)
-        save(img, "hough_warped", drawn_lines)
-
-    else:
+    if not got_hough:
         print("FAILED @ {}, {}, {}, {}".format(180*(h_angl/np.pi), h_thrv, h_minl, h_maxg))
+        save(img, "wcannyfail", img.wcanny)
         exit()
 
-    return lines
+    return vert,hori
 
 def filter_90(img, lines):
     rem = np.empty(lines.shape[0])
@@ -141,20 +149,83 @@ def filter_90(img, lines):
     return lines
 
 def geo_lines(img, lines):
-    print("dummy geo_lines")
     lines = radius_theta(lines)
     vert = lines[abs(lines[:,0,5]) > 45]
     hori = lines[abs(lines[:,0,5]) < 45]
 
-    drawn_lines = cv2.cvtColor(img.warped, cv2.COLOR_GRAY2BGR) * 0
-    draw_lines = cv2.cvtColor(img.warped, cv2.COLOR_GRAY2BGR) * 0
+    vert = vert[vert[:,0,0].argsort()]
+    hori = hori[hori[:,0,1].argsort()]
 
-    for line in vert:
-        for x1,y1,x2,y2,r,t in line:
-            cv2.line(draw_lines,(x1,y1),(x2,y2),(255,0,255),round(2/img.sfact))
-    for line in hori:
-        for x1,y1,x2,y2,r,t in line:
-            cv2.line(draw_lines,(x1,y1),(x2,y2),(0,255,0),round(2/img.sfact))
-    drawn_lines = cv2.addWeighted(img.warped3ch, 0.5, draw_lines, 0.7, 0)
-    save(img, "vert_hori", drawn_lines)
-    return lines
+    return vert, hori
+
+# def get_distances(vert,hori):
+#     distv = np.zeros((vert.shape[0], 2), dtype='int32')
+#     disth = np.zeros((vert.shape[0], 2), dtype='int32')
+
+#     distv[0][0] = abs(vert[1,0,0] - vert[0,0,0])
+#     distv[0][1] = abs(vert[1,0,0] - vert[0,0,0])
+
+#     for i in range (1, vert.shape[0]-1):
+#         distv[i][0] = abs(vert[i-1,0,0] - vert[i,0,0])
+#         distv[i][1] = abs(vert[i+1,0,0] - vert[i,0,0])
+
+#     i += 1
+#     distv[i][0] = abs(vert[i-1,0,0] - vert[i,0,0])
+#     distv[i][1] = abs(vert[i-1,0,0] - vert[i,0,0])
+
+#     disth[0][0] = abs(hori[1,0,0] - hori[0,0,0])
+#     disth[0][1] = abs(hori[1,0,0] - hori[0,0,0])
+
+#     for i in range (1, hori.shape[0]-1):
+#         disth[i][0] = abs(hori[i-1,0,0] - hori[i,0,0])
+#         disth[i][1] = abs(hori[i+1,0,0] - hori[i,0,0])
+
+#     i += 1
+#     disth[i][0] = abs(hori[i-1,0,0] - hori[i,0,0])
+#     disth[i][1] = abs(hori[i-1,0,0] - hori[i,0,0])
+
+#     return distv, disth
+
+def find_intersections(img, lines):
+    inter = []
+    last = (0,0)
+
+    i = 0
+    for x1,y1,x2,y2,r,t in lines:
+        l1 = [(x1,y1), (x2,y2)]
+        j = 0
+        for xx1,yy1,xx2,yy2,rr,tt in lines:
+            l2 =  [(xx1,yy1), (xx2,yy2)]
+            if (x1,y1) == (xx1,yy1) and (x2,y2) == (xx2,yy2):
+                continue
+
+            if abs(t - tt) < 20 or abs(t - tt) > 160:
+                continue
+
+            xdiff = (l1[0][0] - l1[1][0], l2[0][0] - l2[1][0])
+            ydiff = (l1[0][1] - l1[1][1], l2[0][1] - l2[1][1])
+
+            div = determinant(xdiff, ydiff)
+            if div == 0:
+                j += 1
+                continue
+
+            d = (determinant(*l1), determinant(*l2))
+            x = round(determinant(d, xdiff) / div)
+            y = round(determinant(d, ydiff) / div)
+
+            if x > img.wwidth or y > img.wheigth or x < 0 or y < 0:
+                j += 1
+                continue
+            else:
+                j += 1
+                if radius(last[0], last[1], x, y) > 10:
+                    inter.append((x,y))
+                    last = (x,y)
+                else:
+                    # print("Close point ignored: ({},{}) ~ ({},{})".format(last[0],last[1],x,y))
+                    continue
+        i += 1
+
+    inter = np.array(inter, dtype='int32')
+    return inter
