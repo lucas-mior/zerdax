307d9c32ea824db04ef6169166e9965298624d23
lucas-mior <lucas.mior@tutamail.com>
2022-07-30 19:54:04 -0300

    colored

--- src/algo.py
+++ src/algo.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from termcolor import colored, cprint
 
 from Image import Image
 
@@ -12,6 +13,8 @@ def compress_fen(fen):
     """ From: 11111q1k/1111r111/111p1pQP/111P1P11/11prn1R1/11111111/111111P1/R11111K1
         To: 5q1k/4r3/3p1pQP/3P1P2/2prn1R1/8/6P1/R5K1
     """
+    cout = colored("generating compressed FEN...", color="white", attrs=['bold'])
+    print(cout)
     for length in reversed(range(2,9)):
         fen = fen.replace(length * '1', str(length))
     return fen
@@ -60,6 +63,9 @@ def full(filename):
                 fen += '1'
         if i >= 1:
             fen += '/'
+
+    cout = colored("long fen:", color="white", attrs=['bold'])
+    print(cout, fen)
 
     fen = compress_fen(fen)
     return fen
