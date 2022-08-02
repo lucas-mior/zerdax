3e7d213ded80b6435d54734789b7b21274f175af
lucas-mior <lucas.mior@tutamail.com>
2022-08-01 22:28:12 -0300

    draws

--- src/find_board.py
+++ src/find_board.py
@@ -221,9 +221,9 @@ def find_angles(img):
     drawn_lines = cv2.cvtColor(img.hull, cv2.COLOR_GRAY2BGR) * 0
     for line in lines:
         for x1,y1,x2,y2,r,t in line:
-            cv2.line(drawn_lines,(x1,y1),(x2,y2), (0,0,255), 3)
+            cv2.line(drawn_lines,(x1,y1),(x2,y2), color=(0,0,255), 3)
     img.select = drawn_lines[:,:,2]
-    drawn_lines = cv2.addWeighted(img.gray3ch, 0.7, drawn_contours, 0.3, 1)
+    drawn_lines = cv2.addWeighted(img.gray3ch, 0.7, drawn_lines, 0.3, 1)
     save(img, "select", drawn_lines)
 
     img.select_lines = lines
@@ -275,7 +275,7 @@ def find_intersections(img, lines):
     drawn_circles = cv2.cvtColor(img.hull, cv2.COLOR_GRAY2BGR) * 0
     for p in inter:
         cv2.circle(drawn_circles, p, radius=7, color=(255, 0, 0), thickness=-1)
-    drawn_circles = cv2.addWeighted(img.gray3ch, 0.7, drawn_contours, 0.3, 1)
+    drawn_circles = cv2.addWeighted(img.gray3ch, 0.7, drawn_circles, 0.3, 1)
     save(img, "intersections", drawn_circles)
 
     return inter
@@ -326,8 +326,8 @@ def magic_lines(img):
 
     for line in lines:
         for x1,y1,x2,y2,r,t in line:
-            cv2.line(draw_lines,(x1,y1),(x2,y2), (0,0,255), 3)
-    drawn_lines = cv2.addWeighted(img.gray3ch, 0.7, drawn_contours, 0.3, 1)
+            cv2.line(draw_lines,(x1,y1),(x2,y2), color=(0,0,255), 3)
+    drawn_lines = cv2.addWeighted(img.gray3ch, 0.7, drawn_lines, 0.3, 1)
     save(img, "hough_magic", drawn_lines)
 
     dummy = np.copy(img.select_lines[:,:,0:6])
@@ -455,7 +455,7 @@ def calc_corners(img, inter):
     for p in BR, BL, TR, TL:
         cv2.circle(drawn_circles, p, radius=7, color=(0, 255, 0), thickness=-1)
 
-    drawn_circles = cv2.addWeighted(img.gray3ch, 0.7, drawn_contours, 0.3, 1)
+    drawn_circles = cv2.addWeighted(img.gray3ch, 0.7, drawn_circles, 0.3, 1)
     save(img, "corners", drawn_circles)
 
     corners = np.array([BR, BL, TR, TL])
